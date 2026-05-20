f617d580e4cb819f075ef62cad42f6b8cc4fdbfa
Paulo <peekpt@gmail.com>
2026-05-20 14:49:07 +0100

    Reverted to classic jerk and default settings

--- src/Mingda Magician Max Orange/Marlin/Configuration.h
+++ src/Mingda Magician Max Orange/Marlin/Configuration.h
@@ -796,8 +796,8 @@
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
 
-#define DEFAULT_MAX_ACCELERATION { 1500, 1200, 50, 700 }
-// #define DEFAULT_MAX_ACCELERATION      { 600, 600, 100, 700 }
+
+#define DEFAULT_MAX_ACCELERATION      { 600, 600, 100, 700 }
 
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
@@ -814,9 +814,9 @@
  *   M204 T    Travel Acceleration
  */
 
-#define DEFAULT_ACCELERATION          1200   // X, Y, Z and E max acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  1200  // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1200  // X, Y, Z acceleration for travel moves
+#define DEFAULT_ACCELERATION          600   // X, Y, Z and E max acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  600  // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   800  // X, Y, Z acceleration for travel moves
 
 /**
  * Default Jerk limits (mm/s)
@@ -826,11 +826,12 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
-  #define DEFAULT_XJERK 8.0
-  #define DEFAULT_YJERK 5.0
-  #define DEFAULT_ZJERK 0.5
+  #define DEFAULT_XJERK 10.0
+  #define DEFAULT_YJERK 10.0
+  #define DEFAULT_ZJERK 10.0
+
 
   //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
 
@@ -850,7 +851,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.035 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.08 // (mm) Distance from real junction edge
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
